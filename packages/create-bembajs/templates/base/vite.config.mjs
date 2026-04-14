d1bc9c9999f3d567a10b9655be214220d3b8cc21
postacksol <yamikani38@gmail.com>
2026-04-14 11:03:38 +0200

    Update and template update

--- packages/create-bembajs/templates/base/vite.config.mjs
+++ packages/create-bembajs/templates/base/vite.config.mjs
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 import { vitePluginBemba } from 'bembajs-core/vite-plugin-bemba.js';
 
 export default defineConfig({
-  plugins: [vitePluginBemba(), react()],
+  plugins: [vitePluginBemba(), react({ include: [/\.[jt]sx$/, /\.bsx$/] })],
+  esbuild: {
+    include: /src\/.*\.(jsx|bsx|tsx|js|ts)$/,
+    loader: 'jsx'
+  },
   resolve: {
-    extensions: ['.bemba', '.jsx', '.js', '.tsx', '.ts', '.json']
+    extensions: ['.bemba', '.bsx', '.jsx', '.js', '.tsx', '.ts', '.json']
   },
   server: { port: 3000 },
   build: { outDir: 'dist' }
